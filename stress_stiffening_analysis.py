ead06dba8abe306958da68ec91a10df6ecf77004
elhammirzahossein <57906046+elhammirzahossein@users.noreply.github.com>
2020-04-16 13:52:29 +0200

    Update stress_stiffening_analysis.py

--- stress_stiffening_analysis.py
+++ stress_stiffening_analysis.py
@@ -66,24 +66,24 @@ stress_p1=stressfunc(RP_p1*1e-6,-1*1e5)
 stress_p2=stressfunc(RP_p2*1e-6,-2*1e5)
 stress_p3=stressfunc(RP_p3*1e-6,-3*1e5)
 #%%remove bias
-#p1
-index = np.abs(angle_p1)>45#(R*Angle>0) & (R > 50) 
+index = np.abs(RP_p1*angle_p1>0) 
 LA_p1 = copy.deepcopy(longaxis_p1)
 LA_p1[index]=shortaxis_p1[index]
 SA_p1 = copy.deepcopy(shortaxis_p1)
 SA_p1[index]=longaxis_p1[index]
 #p2
-index = np.abs(angle_p2)>45#(R*Angle>0) & (R > 50) 
+index = np.abs(RP_p2*angle_p2>0) 
 LA_p2 = copy.deepcopy(longaxis_p2)
 LA_p2[index]=shortaxis_p2[index]
 SA_p2 = copy.deepcopy(shortaxis_p2)
 SA_p2[index]=longaxis_p2[index]
 #p3
-index = np.abs(angle_p3)>45#(R*Angle>0) & (R > 50) 
+index = np.abs(RP_p3*angle_p3>0)  
 LA_p3 = copy.deepcopy(longaxis_p3)
 LA_p3[index]=shortaxis_p3[index]
 SA_p3 = copy.deepcopy(shortaxis_p3)
 SA_p3[index]=longaxis_p3[index]
+
 #%%  deformation (True strain)
 strain_p1 = (LA_p1 - SA_p1)/np.sqrt(LA_p1 * SA_p1)
 strain_p2 = (LA_p2 - SA_p2)/np.sqrt(LA_p2 * SA_p2)
@@ -111,9 +111,9 @@ ax_size = [0+border_width, 0+border_width,
            1-2*border_width, 1-2*border_width]
 ax2 = fig2.add_axes(ax_size)
 
-ax2.plot(stress_p1,strain_p1,'o')
-ax2.plot(stress_p2,strain_p2,'o')
-ax2.plot(stress_p3,strain_p3,'o')
+ax2.plot(stress_p1,strain_p1,'o',zorder=3)
+ax2.plot(stress_p2,strain_p2,'o',zorder=2)
+ax2.plot(stress_p3,strain_p3,'o',zorder=1)
 ax2.legend(['P = 1 bar','P = 2 bar','P = 3 bar'],loc='upper left')
 ax2.set_xlabel('Stress ,$\u03C3   (Pa) $')
 ax2.set_ylabel('Strain')
@@ -164,6 +164,18 @@ plt.show()
 #    f.write(str(strain[i])+'\t'+str(stress[i])+'\n')
 #f.close()
 #
+
+#%% Coefficient of determination (R^2)
+datapoints=strain_T
+model=fitfunc(stress_T,p[0],p[1])
+
+correlation_matrix = np.corrcoef(datapoints, model)
+correlation_xy = correlation_matrix[0,1]
+r_squared = correlation_xy**2
+
+print('coefficeint of determination =',r_squared)
+#plt.plot(datapoints,model,'o')
+#plt.show()
 
 
 
