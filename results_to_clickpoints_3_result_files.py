e0b7219cb5d19cf072cc98a286492c39d9ad5e78
SelSonntag <selina.sonntag@fau.de>
2020-06-03 13:47:50 +0200

    Result.txt files into clickpoints
    Changed 2 to 3 files

--- results_to_clickpoints_3_result_files.py
+++ results_to_clickpoints_3_result_files.py
@@ -47,18 +47,9 @@ def addEllipses2(data_file2, db,video_file):
     #Frame	x_pos	y_pos	RadialPos	LongAxis	ShortAxis	Angle	irregularity	solidity	sharpness
     #Pathname
     data = np.genfromtxt(data_file2, dtype=float, skip_header=2)    
-    type_elli_good = db.setMarkerType('NN', color='#0000ff', mode=db.TYPE_Ellipse)
-    type_elli_bad = db.setMarkerType('NN', color='#0affe0', mode=db.TYPE_Ellipse)
+    type_elli_good = db.setMarkerType('GT', color='#0affe0', mode=db.TYPE_Ellipse)
     for line in data:
         frame_number = int(line[0])
-        if len(line) == 11:
-            good = line[10]
-        else:
-            good = True
-        if good:
-            ellipse_type = type_elli_good
-        else:
-            ellipse_type = type_elli_bad
             
         db.setEllipse(filename=video_file, 
               frame=frame_number,
@@ -68,13 +59,33 @@ def addEllipses2(data_file2, db,video_file):
               height=line[5] / (config["pixel_size"] * 1e6),
               angle=line[6],
               text=f"{line[6]},{line[4]},{line[5]}",
-              type=ellipse_type)        
+              type=type_elli_good)    
+        
+def addEllipses3(data_file3, db,video_file):
+
+    #Frame	x_pos	y_pos	RadialPos	LongAxis	ShortAxis	Angle	irregularity	solidity	sharpness
+    #Pathname
+    data = np.genfromtxt(data_file3, dtype=float, skip_header=2)    
+    type_elli_good = db.setMarkerType('NN', color='#0000ff', mode=db.TYPE_Ellipse)
+    for line in data:
+        frame_number = int(line[0])            
+        db.setEllipse(filename=video_file, 
+              frame=frame_number,
+              x=line[1],
+              y=line[2],
+              width=line[4] / (config["pixel_size"] * 1e6),
+              height=line[5] / (config["pixel_size"] * 1e6),
+              angle=line[6],
+              text=f"{line[6]},{line[4]},{line[5]}",
+              type=type_elli_good)    
 
 video_file = getInputFile()
-data_file = video_file.replace(".avi", "_result.txt").replace(".tif", "_result_original.txt")
-data_file2 = video_file.replace(".avi", "_result.txt").replace(".tif", "_result.txt")
+data_file = video_file.replace(".avi", "_result.txt").replace(".tif", "_result.txt")
+data_file2 = video_file.replace(".avi", "_result.txt").replace(".tif", "_result_GT_new.txt")
+data_file3 = video_file.replace(".avi", "_result.txt").replace(".tif", "_result_NN_new.txt")
 
-cdb_file = video_file.replace(".avi", ".cdb").replace(".tif", ".cdb")
+
+cdb_file = video_file.replace(".avi", ".cdb").replace(".tif", "_all_ellipses.cdb")
 
 name_ex = os.path.basename(video_file)
 filename_base, file_extension = os.path.splitext(name_ex)
@@ -92,3 +103,5 @@ addVideoToClickpoints(video_file, db)
 # add the ellipses from the results data
 addEllipses(data_file, db,video_file)
 addEllipses2(data_file2, db,video_file)
+addEllipses3(data_file3, db,video_file)
+
