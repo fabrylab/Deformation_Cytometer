7655219de4cf2c55a9ca97b5034a042d00927fa8
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2020-07-20 11:31:00 +0200

    figure_stress_vs_radialposition.py: lump data together

--- figures/figure_stress_vs_radialposition.py
+++ figures/figure_stress_vs_radialposition.py
@@ -93,7 +93,7 @@ def plotPathList(paths):
 datasets = [
     {
         "datafiles": [
-            Path(r"Z:\emirzahossein\data\microscope4_baslercamera\2020_may\2020_05_22_alginateDMEM2%\2"),
+            Path(r"\\131.188.117.96\biophysDS\emirzahossein\data\microscope4_baslercamera\2020_may\2020_05_22_alginateDMEM2%"),
         #    Path(r"Z:\emirzahossein\data\microscope4_baslercamera\2020_july\07_07_2020_alginate2%_rpmi_no_fcs_time"),
         ],
         "rows": 1,
@@ -117,7 +117,7 @@ for data_index, datafile in enumerate(datafiles):
     ax = None
     datafiles = dataset["datafiles"]
     #
-    for index, file in enumerate(Path(datafile).glob("*_result.txt")):
+    for index, file in enumerate(Path(datafile).glob("**/*_result.txt")):
         config = getConfig(file)
         paths.append(file)
         pressures.append(config['pressure_pa'] / 100_000)
@@ -170,7 +170,6 @@ plt.figure(1).ax_dict = {ax.get_label(): ax for ax in plt.figure(1).axes}
 import matplotlib as mpl
 plt.figure(1).set_size_inches(16.260000/2.54, 4.390000/2.54, forward=True)
 plt.figure(1).axes[0].set_position([0.115765, 0.257929, 0.268860, 0.717020])
-plt.figure(1).axes[0].set_xticklabels([""], minor=True)
 plt.figure(1).axes[0].set_xlim(-90.0, 90.0)
 plt.figure(1).axes[0].set_xticklabels(["-75", "0", "75"])
 plt.figure(1).axes[0].set_xticks([-75.0, 0.0, 75.0])
@@ -181,10 +180,13 @@ plt.figure(1).axes[0].text(0.5, 0.5, 'New Text', transform=plt.figure(1).axes[0]
 plt.figure(1).axes[0].texts[0].set_ha("center")
 plt.figure(1).axes[0].texts[0].set_position([0.509148, 0.891045])
 plt.figure(1).axes[0].texts[0].set_text("1 bar")
+plt.figure(1).axes[0].text(0.5, 0.5, 'New Text', transform=plt.figure(1).axes[0].transAxes)  # id=plt.figure(1).axes[0].texts[1].new
+plt.figure(1).axes[0].texts[1].set_position([-0.232258, 0.955913])
+plt.figure(1).axes[0].texts[1].set_text("a")
+plt.figure(1).axes[0].texts[1].set_weight("bold")
 plt.figure(1).axes[0].get_xaxis().get_label().set_text("radial position (µm)")
 plt.figure(1).axes[0].get_yaxis().get_label().set_text("NIH 3T3\nstrain")
 plt.figure(1).axes[1].set_position([0.416266, 0.257929, 0.268860, 0.717020])
-plt.figure(1).axes[1].set_xticklabels([""], minor=True)
 plt.figure(1).axes[1].set_xlim(-90.0, 90.0)
 plt.figure(1).axes[1].set_xticklabels(["-75", "0", "75"])
 plt.figure(1).axes[1].set_xticks([-75.0, 0.0, 75.0])
@@ -199,9 +201,12 @@ plt.figure(1).axes[1].text(0.5, 0.5, 'New Text', transform=plt.figure(1).axes[1]
 plt.figure(1).axes[1].texts[0].set_ha("center")
 plt.figure(1).axes[1].texts[0].set_position([0.545742, 0.891045])
 plt.figure(1).axes[1].texts[0].set_text("2 bar")
+plt.figure(1).axes[1].text(0.5, 0.5, 'New Text', transform=plt.figure(1).axes[1].transAxes)  # id=plt.figure(1).axes[1].texts[1].new
+plt.figure(1).axes[1].texts[1].set_position([-0.127650, 0.955913])
+plt.figure(1).axes[1].texts[1].set_text("b")
+plt.figure(1).axes[1].texts[1].set_weight("bold")
 plt.figure(1).axes[1].get_xaxis().get_label().set_text("radial position (µm)")
 plt.figure(1).axes[2].set_position([0.716769, 0.257929, 0.268860, 0.717020])
-plt.figure(1).axes[2].set_xticklabels([""], minor=True)
 plt.figure(1).axes[2].set_xlim(-90.0, 90.0)
 plt.figure(1).axes[2].set_xticklabels(["-75", "0", "75"])
 plt.figure(1).axes[2].set_xticks([-75.0, 0.0, 75.0])
@@ -215,9 +220,13 @@ plt.figure(1).axes[2].text(0.5, 0.5, 'New Text', transform=plt.figure(1).axes[2]
 plt.figure(1).axes[2].texts[0].set_ha("center")
 plt.figure(1).axes[2].texts[0].set_position([0.504574, 0.891045])
 plt.figure(1).axes[2].texts[0].set_text("3 bar")
+plt.figure(1).axes[2].text(0.5, 0.5, 'New Text', transform=plt.figure(1).axes[2].transAxes)  # id=plt.figure(1).axes[2].texts[1].new
+plt.figure(1).axes[2].texts[1].set_position([-0.110215, 0.955913])
+plt.figure(1).axes[2].texts[1].set_text("c")
+plt.figure(1).axes[2].texts[1].set_weight("bold")
 plt.figure(1).axes[2].get_xaxis().get_label().set_text("radial position (µm)")
 #% end: automatic generated code from pylustrator
-plt.savefig(__file__[:-3]+".png")
+plt.savefig(__file__[:-3]+".png", dpi=300)
 plt.savefig(__file__[:-3]+".pdf")
 plt.show()
 
