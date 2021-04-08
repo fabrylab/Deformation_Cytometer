0f7cd5ed684051db4a6d5ae8a3837e863c3af6e0
Richard Gerum <richard.gerum+dev@protonmail.com>
2021-04-08 23:15:40 +0200

    added figure_time_pressure_etc_THP1.py

--- figures/figure_time_pressure_etc_THP1.py
+++ figures/figure_time_pressure_etc_THP1.py
@@ -11,47 +11,12 @@ import pylustrator
 pylustrator.start()
 
 data, config = load_all_data_new([
-        rf"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope_1\august_2020\2020_08_18_alginate2%_overtime_1\*\*_evaluated_new.csv",
-        rf"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope_1\august_2020\2020_08_18_alginate2%_overtime_2\*\*_evaluated_new.csv",
-        rf"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope_1\august_2020\2020_08_19_alginate2%_overtime_1\*\*_evaluated_new.csv",
-        rf"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope_1\august_2020\2020_08_19_alginate2%_overtime_2\*\*_evaluated_new.csv",
-        #rf"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope4\2020_may\2020_05_22_alginateDMEM2%\*\*_evaluated_new.csv",
-#        rf"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope4\2020_july\2020_07_21_alginate2%_dmem_NIH_time_1\*\*_evaluated_new.csv",
-        rf"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope4\2020_july\2020_07_21_alginate2%_dmem_NIH_time_2\*\*_evaluated_new.csv",
-        rf"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope4\2020_july\2020_07_21_alginate2%_dmem_NIH_time_3\*\*_evaluated_new.csv",
-#        rf"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope4\2020_may\2020_05_22_alginateDMEM2%\*\*_evaluated_new.csv",
-#        rf"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope_1\august_2020\2020_08_18_alginate2%_overtime_1\*\*_evaluated_new.csv",
-#        rf"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope_1\august_2020\2020_08_19_alginate2%_overtime_2\*\*_evaluated_new.csv",
-    ])
-
-data_alg, config = load_all_data_new([
-    r"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\evaluation\diff % alginate\2020_07_24_alginate2.5%_dmem_NIH_3T3\*\*_evaluated_new.csv",
-    r"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\evaluation\diff % alginate\2020_07_27_alginate2.5%_dmem_NIH_3T3\*\*_evaluated_new.csv",
-    # r"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\evaluation\diff % alginate\2020_10_14_alginate2.5%_dmem_NIH_3T3\*\*_evaluated_new.csv",
-    r"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\evaluation\diff % alginate\2020_10_28_alginate2.5%_dmem_NIH_3T3\*\*_evaluated_new.csv",
-    # r"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\evaluation\diff % alginate\2020_10_30_alginate2.5%_dmem_NIH_3T3\*\*_evaluated_new.csv",
-
-    r"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\evaluation\diff % alginate\2020_07_27_alginate2.0%_dmem_NIH_3T3\*\*_evaluated_new.csv",
-    r"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\evaluation\diff % alginate\2020_07_28_alginate2.0%_dmem_NIH_3T3\*\*_evaluated_new.csv",
-    r"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\evaluation\diff % alginate\2020_10_28_alginate2.0%_dmem_NIH_3T3\*\*_evaluated_new.csv",
-    # r"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\evaluation\diff % alginate\2020_10_30_alginate2.0%_dmem_NIH_3T3\*\*_evaluated_new.csv",
-
-    r"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\evaluation\diff % alginate\2020_07_30_alginate1.5%_dmem_NIH_3T3\*\*_evaluated_new.csv",
-    r"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\evaluation\diff % alginate\2020_10_14_alginate1.5%_dmem_NIH_3T3\*\*_evaluated_new.csv",
-    # r"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\evaluation\diff % alginate\2020_10_14_alginate1.5%_dmem_NIH_3T3_2\*\*_evaluated_new.csv",
-    r"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\evaluation\diff % alginate\2020_10_28_alginate1.5%_dmem_NIH_3T3\*\*_evaluated_new.csv"
-    # r"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\evaluation\diff % alginate\2020_10_30_alginate1.5%_dmem_NIH_3T3\*\*_evaluated_new.csv",
-], pressure=2)
-
-data_pos, config = load_all_data_new([
-    fr"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope_1\august_2020\2020_08_14_alginate2%_diffxposition_2\**\*_evaluated_new.csv",
-    fr"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope_1\august_2020\2020_08_21_alginate2%_NIH_xposition_1\**\*_evaluated_new.csv",
-    fr"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope_1\august_2020\2020_08_21_alginate2%_NIH_xposition_2\**\*_evaluated_new.csv",
-    fr"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope_1\august_2020\2020_08_21_alginate2%_NIH_xposition_3\**\*_evaluated_new.csv",
-    fr"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope4\2020_july\2020_07_29_alginate2%_NIH_diff_x_position_2\**\*_evaluated_new.csv",
-    fr"\\131.188.117.96\biophysDS\emirzahossein\microfluidic cell rhemeter data\microscope4\2020_july\2020_07_29_alginate2%_NIH_diff_x_position_3\**\*_evaluated_new.csv",
-], pressure=3)
-
+    fr"\\131.188.117.96\biophysDS\meroles\SPRING2021\24.03.2021_THP1_1.5%Alginate\**\*_evaluated_new.csv",
+    fr"\\131.188.117.96\biophysDS\meroles\SPRING2021\24.03.2021_THP1_2%Alginate\**\*_evaluated_new.csv",
+    fr"\\131.188.117.96\biophysDS\meroles\SPRING2021\25.03.2021_THP1_2.5%Alginate\**\*_evaluated_new.csv",
+    fr"\\131.188.117.96\biophysDS\meroles\SPRING2021\30.03.2021_THP1_Ag2%\**\*_evaluated_new.csv",
+    fr"\\131.188.117.96\biophysDS\meroles\SPRING2021\30.03.2021_THP1_Ag2%_3\**\*_evaluated_new.csv",
+])
 
 
 def plotMeasurementOfParameter(data, parameter, value, color=None):
@@ -67,8 +32,6 @@ def plotMeasurementOfParameter(data, parameter, value, color=None):
     plt.xlabel(parameter)
     plt.ylabel(value)
 
-# filter first measruement
-data = data[data.time >= 10]
 
 def plot_pair(data, parameter):
     plt.axes([0.1, .2, 0.4, 0.5], label=f"{parameter}_alpha")
@@ -76,12 +39,19 @@ def plot_pair(data, parameter):
     plt.axes([0.5, .2, 0.5, 0.5], label=f"{parameter}_k")
     plotMeasurementOfParameter(data, parameter, "w_alpha_cell", "C1")
 
-data_p3 = data[data.pressure.round(2) == 3.0]
+data.pressure = data.pressure.round(3)
+data.alginate = data.alginate.round(3)
+print("pressurs", data.pressure.unique())
+data = data[(data.pressure >= 0.9)*(data.pressure <= 3.1)]
+print("pressurs", data.pressure.unique())
+data_p3 = data[data.pressure == 2.0]
+data_p3 = data_p3[data_p3.alginate == 2.0]
 plot_pair(data_p3, "time")
 
-plot_pair(data, "pressure")
+data_alg2 = data[data.alginate.round(2) == 2.0]
+plot_pair(data_alg2, "pressure")
 
-data_alg_p2 = data_alg[data_alg.pressure.round(2) == 2.0]
+data_alg_p2 = data[data.pressure.round(2) == 2.0]
 #plot_pair(data_alg_p2, "alginate")
 
 d = data_alg_p2
@@ -92,10 +62,12 @@ d25 = d[d.alginate == 2.5]
 d15_, d20_, d25_ = bootstrap_match_hist((d15, d20, d25), 10, 300, "vel_grad_abs")
 plot_pair(pd.concat((d15_, d20_, d25_)), "alginate")
 
-data_pos_p3 = data_pos[data_pos.pressure.round(2) == 3.0]
-plot_pair(data_pos_p3, "pos")
+data_pos_p3 = data[data.alginate == 2.0]
+data_pos_p3 = data_pos_p3[data_pos_p3.pressure == 2.0]
+#plot_pair(data_pos_p3, "pos")
 
-data_p3 = data[data.pressure.round(2) == 3.0].copy()
+data_p3 = data[data.pressure == 2.0].copy()
+data_p3 = data_p3[data_p3.alginate == 2.0]
 bins = np.percentile(data_p3.area, [0, 33, 66, 100])
 print("cell size, bins", bins)
 data_p3["cell_size"] = "1small"
@@ -114,12 +86,12 @@ plt.figure(1).ax_dict["alginate_alpha"].set_ylim(0.0, 190.0)
 plt.figure(1).ax_dict["alginate_alpha"].set_yticks([0.0, 50.0, 100.0, 150.0, np.nan])
 plt.figure(1).ax_dict["alginate_alpha"].set_xticklabels([])
 plt.figure(1).ax_dict["alginate_alpha"].set_yticklabels(["", "", "", "", ""], fontsize=10.0, fontweight="normal", color="black", fontstyle="normal", fontname="Arial", horizontalalignment="right")
-plt.figure(1).ax_dict["alginate_alpha"].set_position([0.671119, 0.594697, 0.146365, 0.329480])
+plt.figure(1).ax_dict["alginate_alpha"].set_position([0.527228, 0.597512, 0.173844, 0.329480])
 plt.figure(1).ax_dict["alginate_alpha"].spines['right'].set_visible(False)
 plt.figure(1).ax_dict["alginate_alpha"].spines['top'].set_visible(False)
 plt.figure(1).ax_dict["alginate_alpha"].text(0.5, 0.5, 'New Text', transform=plt.figure(1).ax_dict["alginate_alpha"].transAxes)  # id=plt.figure(1).ax_dict["alginate_alpha"].texts[0].new
 plt.figure(1).ax_dict["alginate_alpha"].texts[0].set_position([-0.165224, 1.038176])
-plt.figure(1).ax_dict["alginate_alpha"].texts[0].set_text("g")
+plt.figure(1).ax_dict["alginate_alpha"].texts[0].set_text("e")
 plt.figure(1).ax_dict["alginate_alpha"].texts[0].set_weight("bold")
 plt.figure(1).ax_dict["alginate_alpha"].get_xaxis().get_label().set_text('')
 plt.figure(1).ax_dict["alginate_alpha"].get_yaxis().get_label().set_text("")
@@ -127,12 +99,12 @@ plt.figure(1).ax_dict["alginate_k"].set_xlim(1.3952195212681158, 2.6032812679138
 plt.figure(1).ax_dict["alginate_k"].set_ylim(0.0, 0.45)
 plt.figure(1).ax_dict["alginate_k"].set_yticks([0.0, 0.1, 0.2, 0.3, 0.4])
 plt.figure(1).ax_dict["alginate_k"].set_yticklabels(["", "", "", "", ""], fontsize=10.0, fontweight="normal", color="black", fontstyle="normal", fontname="Arial", horizontalalignment="right")
-plt.figure(1).ax_dict["alginate_k"].set_position([0.671119, 0.193851, 0.146365, 0.329480])
+plt.figure(1).ax_dict["alginate_k"].set_position([0.529005, 0.196666, 0.173844, 0.329480])
 plt.figure(1).ax_dict["alginate_k"].spines['right'].set_visible(False)
 plt.figure(1).ax_dict["alginate_k"].spines['top'].set_visible(False)
 plt.figure(1).ax_dict["alginate_k"].text(0.5, 0.5, 'New Text', transform=plt.figure(1).ax_dict["alginate_k"].transAxes)  # id=plt.figure(1).ax_dict["alginate_k"].texts[0].new
 plt.figure(1).ax_dict["alginate_k"].texts[0].set_position([-0.165224, 0.974992])
-plt.figure(1).ax_dict["alginate_k"].texts[0].set_text("h")
+plt.figure(1).ax_dict["alginate_k"].texts[0].set_text("f")
 plt.figure(1).ax_dict["alginate_k"].texts[0].set_weight("bold")
 plt.figure(1).ax_dict["alginate_k"].get_xaxis().get_label().set_text("alginate %")
 plt.figure(1).ax_dict["alginate_k"].get_yaxis().get_label().set_text('')
@@ -142,12 +114,12 @@ plt.figure(1).ax_dict["cell_size_alpha"].set_xticks([0.0, 1.0, 2.0])
 plt.figure(1).ax_dict["cell_size_alpha"].set_yticks([0.0, 50.0, 100.0, 150.0, np.nan])
 plt.figure(1).ax_dict["cell_size_alpha"].set_xticklabels(["", "", ""], fontsize=10.0, fontweight="normal", color="black", fontstyle="normal", fontname="Arial", horizontalalignment="center")
 plt.figure(1).ax_dict["cell_size_alpha"].set_yticklabels(["", "", "", "", ""], fontsize=10.0, fontweight="normal", color="black", fontstyle="normal", fontname="Arial", horizontalalignment="right")
-plt.figure(1).ax_dict["cell_size_alpha"].set_position([0.845430, 0.594697, 0.146365, 0.329480])
+plt.figure(1).ax_dict["cell_size_alpha"].set_position([0.733013, 0.597512, 0.173844, 0.329480])
 plt.figure(1).ax_dict["cell_size_alpha"].spines['right'].set_visible(False)
 plt.figure(1).ax_dict["cell_size_alpha"].spines['top'].set_visible(False)
 plt.figure(1).ax_dict["cell_size_alpha"].text(0.5, 0.5, 'New Text', transform=plt.figure(1).ax_dict["cell_size_alpha"].transAxes)  # id=plt.figure(1).ax_dict["cell_size_alpha"].texts[0].new
 plt.figure(1).ax_dict["cell_size_alpha"].texts[0].set_position([-0.128998, 1.038176])
-plt.figure(1).ax_dict["cell_size_alpha"].texts[0].set_text("i")
+plt.figure(1).ax_dict["cell_size_alpha"].texts[0].set_text("g")
 plt.figure(1).ax_dict["cell_size_alpha"].texts[0].set_weight("bold")
 plt.figure(1).ax_dict["cell_size_alpha"].get_xaxis().get_label().set_text("")
 plt.figure(1).ax_dict["cell_size_alpha"].get_yaxis().get_label().set_text("")
@@ -157,51 +129,21 @@ plt.figure(1).ax_dict["cell_size_k"].set_xticks([0.0, 1.0, 2.0])
 plt.figure(1).ax_dict["cell_size_k"].set_yticks([0.0, 0.1, 0.2, 0.3, 0.4])
 plt.figure(1).ax_dict["cell_size_k"].set_xticklabels(["small", "medium", "big"], fontsize=10.0, fontweight="normal", color="black", fontstyle="normal", fontname="Arial", horizontalalignment="center")
 plt.figure(1).ax_dict["cell_size_k"].set_yticklabels(["", "", "", "", ""], fontsize=10.0, fontweight="normal", color="black", fontstyle="normal", fontname="Arial", horizontalalignment="right")
-plt.figure(1).ax_dict["cell_size_k"].set_position([0.845430, 0.196150, 0.146365, 0.329480])
+plt.figure(1).ax_dict["cell_size_k"].set_position([0.734791, 0.198965, 0.173844, 0.329480])
 plt.figure(1).ax_dict["cell_size_k"].spines['right'].set_visible(False)
 plt.figure(1).ax_dict["cell_size_k"].spines['top'].set_visible(False)
 plt.figure(1).ax_dict["cell_size_k"].text(0.5, 0.5, 'New Text', transform=plt.figure(1).ax_dict["cell_size_k"].transAxes)  # id=plt.figure(1).ax_dict["cell_size_k"].texts[0].new
 plt.figure(1).ax_dict["cell_size_k"].texts[0].set_position([-0.128998, 0.968015])
-plt.figure(1).ax_dict["cell_size_k"].texts[0].set_text("j")
+plt.figure(1).ax_dict["cell_size_k"].texts[0].set_text("h")
 plt.figure(1).ax_dict["cell_size_k"].texts[0].set_weight("bold")
 plt.figure(1).ax_dict["cell_size_k"].get_xaxis().get_label().set_text("cell size")
 plt.figure(1).ax_dict["cell_size_k"].get_yaxis().get_label().set_text("")
-plt.figure(1).ax_dict["pos_alpha"].set_xlim(-0.14855274904467136, 2.1535578799008785)
-plt.figure(1).ax_dict["pos_alpha"].set_ylim(0.0, 190.0)
-plt.figure(1).ax_dict["pos_alpha"].set_xticks([0.0, 1.0, 2.0])
-plt.figure(1).ax_dict["pos_alpha"].set_yticks([0.0, 50.0, 100.0, 150.0, np.nan])
-plt.figure(1).ax_dict["pos_alpha"].set_xticklabels(["", "", ""], fontsize=10.0, fontweight="normal", color="black", fontstyle="normal", fontname="Arial", horizontalalignment="center")
-plt.figure(1).ax_dict["pos_alpha"].set_yticklabels(["", "", "", "", ""], fontsize=10.0, fontweight="normal", color="black", fontstyle="normal", fontname="Arial", horizontalalignment="right")
-plt.figure(1).ax_dict["pos_alpha"].set_position([0.496807, 0.592783, 0.146365, 0.329480])
-plt.figure(1).ax_dict["pos_alpha"].spines['right'].set_visible(False)
-plt.figure(1).ax_dict["pos_alpha"].spines['top'].set_visible(False)
-plt.figure(1).ax_dict["pos_alpha"].text(0.5, 0.5, 'New Text', transform=plt.figure(1).ax_dict["pos_alpha"].transAxes)  # id=plt.figure(1).ax_dict["pos_alpha"].texts[0].new
-plt.figure(1).ax_dict["pos_alpha"].texts[0].set_position([-0.101886, 1.043986])
-plt.figure(1).ax_dict["pos_alpha"].texts[0].set_text("e")
-plt.figure(1).ax_dict["pos_alpha"].texts[0].set_weight("bold")
-plt.figure(1).ax_dict["pos_alpha"].get_xaxis().get_label().set_text("")
-plt.figure(1).ax_dict["pos_alpha"].get_yaxis().get_label().set_text("")
-plt.figure(1).ax_dict["pos_k"].set_xlim(-0.1339748743572315, 2.135578353332946)
-plt.figure(1).ax_dict["pos_k"].set_ylim(0.0, 0.45)
-plt.figure(1).ax_dict["pos_k"].set_xticks([0.0, 1.0, 2.0])
-plt.figure(1).ax_dict["pos_k"].set_yticks([0.0, 0.1, 0.2, 0.3, 0.4])
-plt.figure(1).ax_dict["pos_k"].set_xticklabels(["inlet", "middle", "outlet"], fontsize=10.0, fontweight="normal", color="black", fontstyle="normal", fontname="Arial", horizontalalignment="center")
-plt.figure(1).ax_dict["pos_k"].set_yticklabels(["", "", "", "", ""], fontsize=10.0, fontweight="normal", color="black", fontstyle="normal", fontname="Arial", horizontalalignment="right")
-plt.figure(1).ax_dict["pos_k"].set_position([0.496807, 0.196150, 0.146365, 0.329480])
-plt.figure(1).ax_dict["pos_k"].spines['right'].set_visible(False)
-plt.figure(1).ax_dict["pos_k"].spines['top'].set_visible(False)
-plt.figure(1).ax_dict["pos_k"].text(0.5, 0.5, 'New Text', transform=plt.figure(1).ax_dict["pos_k"].transAxes)  # id=plt.figure(1).ax_dict["pos_k"].texts[0].new
-plt.figure(1).ax_dict["pos_k"].texts[0].set_position([-0.111824, 0.969543])
-plt.figure(1).ax_dict["pos_k"].texts[0].set_text("f")
-plt.figure(1).ax_dict["pos_k"].texts[0].set_weight("bold")
-plt.figure(1).ax_dict["pos_k"].get_xaxis().get_label().set_text("channel pos")
-plt.figure(1).ax_dict["pos_k"].get_yaxis().get_label().set_text("")
 plt.figure(1).ax_dict["pressure_alpha"].set_xlim(0.5, 3.5)
 plt.figure(1).ax_dict["pressure_alpha"].set_ylim(0.0, 190.0)
 plt.figure(1).ax_dict["pressure_alpha"].set_yticks([0.0, 50.0, 100.0, 150.0, np.nan])
 plt.figure(1).ax_dict["pressure_alpha"].set_xticklabels([])
 plt.figure(1).ax_dict["pressure_alpha"].set_yticklabels(["", "", "", "", ""], fontsize=10.0, fontweight="normal", color="black", fontstyle="normal", fontname="Arial", horizontalalignment="right")
-plt.figure(1).ax_dict["pressure_alpha"].set_position([0.322496, 0.594697, 0.146365, 0.329480])
+plt.figure(1).ax_dict["pressure_alpha"].set_position([0.321443, 0.594697, 0.173844, 0.329480])
 plt.figure(1).ax_dict["pressure_alpha"].spines['right'].set_visible(False)
 plt.figure(1).ax_dict["pressure_alpha"].spines['top'].set_visible(False)
 plt.figure(1).ax_dict["pressure_alpha"].text(0.5, 0.5, 'New Text', transform=plt.figure(1).ax_dict["pressure_alpha"].transAxes)  # id=plt.figure(1).ax_dict["pressure_alpha"].texts[0].new
@@ -214,7 +156,7 @@ plt.figure(1).ax_dict["pressure_k"].set_xlim(0.5, 3.5)
 plt.figure(1).ax_dict["pressure_k"].set_ylim(0.0, 0.45)
 plt.figure(1).ax_dict["pressure_k"].set_yticks([0.0, 0.1, 0.2, 0.3, 0.4])
 plt.figure(1).ax_dict["pressure_k"].set_yticklabels(["", "", "", "", ""], fontsize=10.0, fontweight="normal", color="black", fontstyle="normal", fontname="Arial", horizontalalignment="right")
-plt.figure(1).ax_dict["pressure_k"].set_position([0.322496, 0.193851, 0.146365, 0.329480])
+plt.figure(1).ax_dict["pressure_k"].set_position([0.321442, 0.193851, 0.173844, 0.329480])
 plt.figure(1).ax_dict["pressure_k"].spines['right'].set_visible(False)
 plt.figure(1).ax_dict["pressure_k"].spines['top'].set_visible(False)
 plt.figure(1).ax_dict["pressure_k"].text(0.5, 0.5, 'New Text', transform=plt.figure(1).ax_dict["pressure_k"].transAxes)  # id=plt.figure(1).ax_dict["pressure_k"].texts[0].new
@@ -250,6 +192,10 @@ plt.figure(1).ax_dict["time_alpha"].text(0.5, 0.5, 'New Text', transform=plt.fig
 plt.figure(1).ax_dict["time_k"].texts[0].set_position([-0.258550, 1.038117])
 plt.figure(1).ax_dict["time_k"].texts[0].set_text("a")
 plt.figure(1).ax_dict["time_k"].texts[0].set_weight("bold")
+plt.figure(1).ax_dict["time_k"].text(0.5, 0.5, 'New Text', transform=plt.figure(1).ax_dict["time_k"].transAxes)  # id=plt.figure(1).ax_dict["time_k"].texts[1].new
+plt.figure(1).ax_dict["time_k"].texts[1].set_position([-0.258545, 0.966501])
+plt.figure(1).ax_dict["time_k"].texts[1].set_text("b")
+plt.figure(1).ax_dict["time_k"].texts[1].set_weight("bold")
 plt.figure(1).ax_dict["time_k"].get_xaxis().get_label().set_text("time (min)")
 plt.figure(1).ax_dict["time_k"].get_yaxis().get_label().set_text("alpha")
 #% end: automatic generated code from pylustrator
